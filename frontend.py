d1f75210ca14c76af4255e401b530ceb98336b43
Torkuno <tmesalles@hotmail.com>
2023-11-27 22:29:11 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,75 +1,52 @@
 import streamlit as st
 import time
 
-# HEAD ----------------------------
 st.set_page_config(
     page_title="Valve Dashboard",
     page_icon="👋",
 )
 
-
-# BODY ----------------------------
-
 # Function to send command to the backend
 def send_valve_command(valveId, command):
-    # Here you would add the logic to send a command to your backend,
-    # which would then communicate with Azure IoT Hub
+    # Add logic to send commands to backend and Azure IoT Hub
     pass
 
 def generate_temporary_success(message, seconds):
-    # generate the success
-    successContainer = st.success(message)
-    # wait amout of time
-    time.sleep(seconds)
-    # elimilate success
-    successContainer.empty()
+    successContainer = st.success(message)  # generate the success
+    time.sleep(seconds)  # wait
+    successContainer.empty()  # eliminate success
 
-# tab title
 st.title('Valve Control Dashboard')
 
-# create the valveUpdate variable to 
-# avoid a specific bug
 valveUpdate = ''
 
-# declare the variables for valve status
+# Valve states dictionary
 valves = {
-    'Valve 1': 'closed', 
-    'Valve 2': 'closed', 
-    'Valve 3': 'closed', 
-    }
+    'Valve 1': 'closed',
+    'Valve 2': 'closed',
+    'Valve 3': 'closed',
+    'Valve 4': 'closed',
+}
 
-# Make columns for UX design
+# Make columns
 col1, col2 = st.columns((1, 1))
 
-# LEFT COL. The selectbox and Open / Close buttons
+# LEFT COLUMN: The selectbox and Open / Close buttons
 with col1:
-    # Create a selectbox element which contains the valves
-    valveId = st.selectbox('Select Valve', ['Valve 1', 'Valve 2', 'Valve 3'])
+    valveId = st.selectbox('Select Valve', ['Valve 1', 'Valve 2', 'Valve 3', 'Valve 4'])
     btnOpenValve = st.button('Open Valve')
     btnCloseValve = st.button('Close Valve')
 
-    # the open button is pressed
-    if btnOpenValve:
-        # send_valve_command() is used in the backend
-
+    if btnOpenValve:  # the open button is pressed
         send_valve_command(valveId, 'open')
-        # set the corresponding valve to open
-        valves[valveId] = 'open'
-        valveUpdate = f'The valve is {valves[valveId]}'
-    
-    # the close button is pressed
-    if btnCloseValve:
-        # send_valve_command() is used in the backend
-        
-        send_valve_command(valveId, 'closed')
-        # set the corresponding valve to closed
-        valves[valveId] = 'closed'
-        valveUpdate = f'The valve is {valves[valveId]}'
-    
-# RIGHT COL. Valve status and success message
-with col2:
-    # print the status of the corresponding valve
-    st.markdown(f'**Status: {valves[valveId]}**')
-    # print success message after either button
-    generate_temporary_success(valveUpdate, 1)
+        valves[valveId] = 'open'  # set the corresponding valve to open
 
+    if btnCloseValve:  # the close button is pressed
+        send_valve_command(valveId, 'closed')
+        valves[valveId] = 'closed'  # set the corresponding valve to closed
+
+# RIGHT COLUMN: Valve status and success message
+with col2:
+    st.markdown(f'**Status: {valves[valveId]}**')  # print the status of the corresponding valve
+    valveUpdate = f'The valve is {valves[valveId]}'
+    generate_temporary_success(valveUpdate, 1)  # print success message after either button
