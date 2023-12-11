68f77c4eda5266b4d667e55a3719ec8e81b52645
Torkuno <tmesalles@hotmail.com>
2023-12-11 23:40:21 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from azure.iot.device import IoTHubDeviceClient, Message
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import json
 
 # HEAD
 st.set_page_config(
@@ -8,18 +10,22 @@ st.set_page_config(
 )
 
 pipeCS = {
-    "pipe1": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-1;SharedAccessKey=d8LOzgbhgU31Z7k09ILmTXADBp1XHDYy/AIoTKtXyA4=",
-    "pipe2": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-2;SharedAccessKey=t6ZwKzDn8K/iGgyJvktZPCA/AB2R4KBOLAIoTONAnvQ=",
-    "pipe3": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-3;SharedAccessKey=a9Erx9/+KhWOm/mAWRzeE0fiSVFyrNG6GAIoTNPCCo4=",
-    "pipe4": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-4;SharedAccessKey=y2PSh29338hh8gtrK8PTEqfp5dvaGH4mjAIoTKZ/CWY="
+    "valve-1": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-1;SharedAccessKey=d8LOzgbhgU31Z7k09ILmTXADBp1XHDYy/AIoTKtXyA4=",
+    "valve-2": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-2;SharedAccessKey=t6ZwKzDn8K/iGgyJvktZPCA/AB2R4KBOLAIoTONAnvQ=",
+    "valve-3": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-3;SharedAccessKey=a9Erx9/+KhWOm/mAWRzeE0fiSVFyrNG6GAIoTNPCCo4=",
+    "valve-4": "HostName=cloud-final-project.azure-devices.net;DeviceId=valve-4;SharedAccessKey=y2PSh29338hh8gtrK8PTEqfp5dvaGH4mjAIoTKZ/CWY="
     }
 
 pipeStates = {
-    'pipe1': 'closed',
-    'pipe2': 'closed',
-    'pipe3': 'closed',
-    'pipe4': 'closed'
+    'valve-1': 'closed',
+    'valve-2': 'closed',
+    'valve-3': 'closed',
+    'valve-4': 'closed'
 }
+
+storage_connection_string = "DefaultEndpointsProtocol=https;AccountName=valvestatus;AccountKey=jjwKA+WoOe6BtHufikuu3gXpd8tksXWrMRY7txb9MUTA6nwKTd9VTQK7Mdpo+iZabzZbIz6jsWVh+ASt8vvNZA==;EndpointSuffix=core.windows.net"
+container_names = ["valve-1", "valve-2", "valve-3", "valve-4"]
+blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
 
 def message_to_hub(whichPipe: str, condition: str):
     # connecting to IoT hub directly from Streamlit
@@ -34,9 +40,16 @@ def message_to_hub(whichPipe: str, condition: str):
     # sending message
     client.send_message(Message(str(message_body)))
 
-    st.success(f"Message {message_body} sent to {whichPipe}")
+    st.success(f"Condition: {condition} sent to {whichPipe}")
 
     client.disconnect()
+
+    json_string = json.dumps(message_body)
+    blob_name = "sensor_data.json"
+    blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
+    blob_client = blob_service_client.get_blob_client(container=whichPipe, blob=blob_name)
+    blob_client.upload_blob(json_string, overwrite=True)
+
 
 
 # BODY
@@ -68,3 +81,12 @@ with col2:
     if 'pipeUpdate' in st.session_state:
         st.success(st.session_state['pipeUpdate'])
         del st.session_state['pipeUpdate']  # Clear the success message after displaying
+
+    for container_name in container_names:
+        container_client = blob_service_client.get_container_client(container_name)
+        blobs = container_client.list_blobs()
+        latest_blob = max(blobs, key=lambda blob: blob.last_modified)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=latest_blob.name)
+        blob_content = blob_client.download_blob().readall()
+        json_data = json.loads(blob_content)
+        st.text(f"{container_name}: {json_data}")
